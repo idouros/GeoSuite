ddbab246dff3fb42ce0bda5778c3e8a2c2645351
Yannis Douros <yannis@douros.org>
2020-08-26 19:21:44 +0300

    GridMesh output to file.

--- apps/LandscapeGenerator/LandscapeGenerator.cpp
+++ apps/LandscapeGenerator/LandscapeGenerator.cpp
@@ -7,18 +7,19 @@
 
 int main()
 {
-    auto a = MeshCode();
 
-    std::cout << "Hello World!\n";
+	ofstream outfile;
+	outfile.open("W:\\Data\\landscape_01.obj");
+
+	GridMesh gm = { 
+		{ 0, 0, 0 ,0 }, 
+		{ 0, 10, 0, 0 },
+		{ 0, 0, 0 ,0 },
+		{ 0, 0, 0 ,0 }
+		};
+
+	SaveAsObjFile(gm, 10, outfile);
+	outfile.close();
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

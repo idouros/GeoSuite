f003aa7a5bdbb3643e411161bd60fa63c3536272
Yannis Douros <yannis@douros.org>
2020-09-04 16:36:48 +0300

    This definitely looks like landscapes already.

--- apps/LandscapeGenerator/LandscapeGenerator.cpp
+++ apps/LandscapeGenerator/LandscapeGenerator.cpp
@@ -8,7 +8,11 @@ int main()
 	LandscapeParams p;
 
 	// TODO: read the params from config file
-	p.scale = 10;
+	p.grid_scale = 10.0;
+	p.grid_rows = 500;
+	p.grid_cols = 1000;
+	p.min_altitude = 1.0;
+	p.max_altitude = 2000.0;
 	std::string outFileName = "W:\\Data\\landscape_01.obj";
 	
 	// Create it

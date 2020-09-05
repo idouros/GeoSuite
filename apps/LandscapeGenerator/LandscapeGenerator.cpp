705254c6bfc53b50cea836af6db16e22979c1842
Yannis Douros <yannis@douros.org>
2020-09-05 16:03:35 +0300

    Submodule update.

--- apps/LandscapeGenerator/LandscapeGenerator.cpp
+++ apps/LandscapeGenerator/LandscapeGenerator.cpp
@@ -8,20 +8,22 @@ int main()
 	LandscapeParams p;
 
 	// TODO: read the params from config file
-	p.grid_scale = 75.0;
-	p.grid_rows = 300;
-	p.grid_cols = 600;
+	p.grid_scale = 100.0;
+	p.grid_rows = 500;
+	p.grid_cols = 500;
 	p.min_altitude = 0.0;
-	p.max_altitude = 2918.0;
-	p.variance = 350.0;
-	std::string outFileName = "W:\\Data\\landscape_01.obj";
+	p.max_altitude = 5000.0;
+	p.variance = 2750.0;
+	p.variance_fade = 0.3;
+	p.last_random_pass = 2;
+	p.out_file_name = "W:\\Data\\landscape_01.obj";
 	
 	// Create it
 	auto gm = GeoMesh::CreateGeoMesh(p);
 
 	// Save
 	std::ofstream outFile;
-	outFile.open(outFileName);
+	outFile.open(p.out_file_name);
 	gm->SaveAsObjFile(outFile);
 	outFile.close();
 }
